a44a989e491f049a9969c6924af70244560fc3ce
Nathália <np.nathaliapereira@gmail.com>
2022-04-29 17:51:48 -0300

    feat: Implementa tela inicial
    Com três tabelas para o preenchimento dos dados

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,80 @@
+import { Checkbox, TextField, Button } from '@mui/material';
+
+import MainTable from '../../components/MainTable';
+import SelectionBox from '../../components/SelectionBox';
+import { Container, Content, TableContainer, Title, TableTitle, TableContent, ButtonContainer } from './styles';
+
 const Home = () => {
+
+  function createSelection(
+    name: string,
+    value: number,
+  ) {
+    return { name, value };
+  }
+
+  const rowsGender = [
+    createSelection('female', 1),
+    createSelection('male', 2),
+  ];
+
+  const rowsLevel = [
+    createSelection('normal', 1),
+    createSelection('above normal', 2),
+    createSelection('well above normal', 3),
+  ];
+
+  function createRow(
+    name: string,
+    body: any,
+  ) {
+    return { name, body };
+  }
+
+  const rowsSubjective = [
+    createRow('Smoking', <Checkbox />),
+    createRow('Alcohol intake', <Checkbox />),
+    createRow('Physical activity', <Checkbox />),
+  ];
+
+  const rowsObjective = [
+    createRow('Age', <TextField required id="outlined-basic" type="number" label="years" variant="outlined" />),
+    createRow('Height', <TextField required id="outlined-basic" type="number" label="cm" variant="outlined" />),
+    createRow('Weight', <TextField required id="outlined-basic" type="number" label="kg" variant="outlined" />),
+    createRow('Gender', <SelectionBox label='gender' rows={rowsGender}></SelectionBox>),
+  ];
+
+  const rowsExamination = [
+    createRow('Systolic blood pressure', <TextField required id="outlined-basic" type="number" label="years" variant="outlined" />),
+    createRow('Diastolic blood pressure', <TextField required id="outlined-basic" type="number" label="cm" variant="outlined" />),
+    createRow('Cholesterol', <SelectionBox label='level' rows={rowsLevel}></SelectionBox>),
+    createRow('Glucose', <SelectionBox label='level' rows={rowsLevel}></SelectionBox>),
+  ];
+
   return (
-    <h1>Oi</h1>
+    <Container>
+      <Title>Risco de doenças cardiovasculares</Title>
+      <h1>Em risco</h1>
+      <Content>
+        <TableContainer>
+          <TableContent>
+            <TableTitle>Objective</TableTitle>
+            <MainTable title='Objective' rows={rowsObjective}></MainTable>
+          </TableContent>
+          <TableContent>
+            <TableTitle>Examination</TableTitle>
+            <MainTable title='Examination' rows={rowsExamination}></MainTable>
+          </TableContent>
+          <TableContent>
+            <TableTitle>Subjective</TableTitle>
+            <MainTable title='Subjective' rows={rowsSubjective}></MainTable>
+            <ButtonContainer>
+            <Button variant="contained">Calculate</Button>
+            </ButtonContainer>
+          </TableContent>
+        </TableContainer>
+      </Content>
+    </Container>
   );
 }
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,4 +2,56 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
-`
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: 80%;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  font-size: 30px;
+  font-family: 'Roboto';
+  height: 20%;
+  padding: 2rem;
+`;
+
+export const TableContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+export const TableContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 55%;
+  height: 100%;
+  padding: 1.5rem;
+`;
+
+export const TableTitle = styled.div`
+  font-size: 30px;
+  padding: 2rem;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  padding: 2rem;
+`;
