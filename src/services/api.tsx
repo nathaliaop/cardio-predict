ff729c926f393d3bfbeadb6abeb3d5e25e2672f8
Nathália <np.nathaliapereira@gmail.com>
2022-04-30 06:47:04 -0300

    feat: Adiciona a url da API como uma variável de ambiente

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-// process.env.URL
 const api = axios.create({
-    baseURL: 'https://cardio-disease-predict.herokuapp.com/',
+    baseURL: process.env.API_URL,
     headers: {
         'Content-Type': 'application/json',
     }
